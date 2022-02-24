55eace82806c374eddc59946b08f9166c012cba1
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2022-02-24 14:59:03 -0600

    update test.py

--- test/test.py
+++ test/test.py
@@ -11,6 +11,21 @@ class Test(Cog):
     @commands.command()
     async def test(self, ctx):
         await ctx.send("Test")
+    
+    @commands.command()
+    async def say(self, ctx, *, arg=None):
+        if arg == None:
+            embed = discord.Embed(
+                color=0xFF0000
+                )  
+            embed.add_field(name='Error', value="Please specify what you want me to say.", inline=False)
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(
+                color=0x2ECC71
+                )  
+            embed.add_field(name=f'{ctx.message.author.name}', value=f"{arg}", inline=False)
+            await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Test(bot))
