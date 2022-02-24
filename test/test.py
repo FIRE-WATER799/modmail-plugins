0be9b851ef980764d1e0b393391a6a00204e6798
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2022-02-24 14:48:37 -0600

    update test.py

--- test/test.py
+++ test/test.py
@@ -1,1 +1,11 @@
+import discord
+from discord.ext import commands
 
+class Test(Cog):
+    def __init__(self,bot):
+        super().__init__()
+        self.bot = bot
+    
+    @commands.command()
+    async def test(self, ctx):
+        await ctx.send("Test")
