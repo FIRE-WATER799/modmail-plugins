bcce431aebd019630b13c27b073c355c2b898cae
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2022-06-10 20:46:44 -0500

    update dev.py

---
+++ dev/dev.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands, Webhook, RequestsWebhookAdapter
+
+
+Cog = getattr(commands, "Cog", object)
+
+class Dev(Cog):
+    def __init__(self,bot):
+        super().__init__()
+        self.bot = bot
+    
+    @commands.command()
+    async def test(self, ctx):
+        await ctx.send("Test")
+    
+    @commands.command()
+    async def review(self, ctx, *, arg=None :int):
+        if arg == None:
+            await ctx.send("You did not provide a rating")
+        else:
+            webhook = Webhook.from_url('webhook-url-here', adapter=RequestsWebhookAdapter())
+            rate = discord.Embed(title="Rating")
+            rate.add_field(name="Name", value=self.bot.user.name)
+            rate.add_field(name="Rating", value = str(arg) + "This is the users rating")
+            webhook.send(content="Hello World")
+
+def setup(bot):
+    bot.add_cog(Dev(bot))
